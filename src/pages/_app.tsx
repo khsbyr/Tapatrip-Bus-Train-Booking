e77b42463f48b8aa462d1dd0173163750546560f
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-10-21 12:12:49 +0800

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,13 @@
 import { FC, useEffect } from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import theme from '@context/theme';
 
 import '@assets/chrome-bug.scss';
 import '@assets/main.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles?.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import theme from '@context/theme';
 
-// https://mui.com/styles/advanced/#next-js
-export default class MyDocument extends Document {
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
-          {/* PWA primary color */}
-          <meta content={theme.palette.primary.main} name="theme-color" />
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>
@@ -25,28 +32,4 @@ export default class MyDocument extends Document {
   }
 }
 
-// https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
-MyDocument.getInitialProps = async ctx => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <React.Fragment>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </React.Fragment>
-      ),
-    };
-  } finally {
-    sheet.seal();
-  }
-};
+export default MyDocument;
