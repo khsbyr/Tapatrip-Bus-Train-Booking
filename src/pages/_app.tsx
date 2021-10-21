39d20f6705145f79841c0cbaea55cb759978ff9f
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-10-21 12:19:32 +0800

    Ficx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import type { AppProps } from 'next/app';
-
+import 'antd/dist/antd.css';
 import '@assets/chrome-bug.scss';
 import '@assets/main.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
