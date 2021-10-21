49c559e0234b228f70e8f5ccff982dcc72d7321b
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-10-21 12:23:44 +0800

    Fix

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const About = () => {
+  return (
+    <div>
+      <div>abbbbb</div>
+      <Button>Default Button</Button>
+    </div>
+  );
+};
+
+export default About;
