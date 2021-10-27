d88925ff344013c1384781de9dc66e6904c1fb06
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-10-27 15:09:06 +0800

    Fix

--- src/pages/bus/index.tsx
+++ src/pages/bus/index.tsx
@@ -6,7 +6,7 @@ import NavData from '@data/navData.json';
 import TapaServiceList from '@data/tapaServiceList.json';
 import Layout from '@components/common/Layout';
 import Search from '@components/bus/Search/Search';
-import Navbar from '@components/common/Navbar/Navbar';
+import Navbar from '@components/common/Navbar';
 import TapaService from '@components/common/TapaService';
 import Subscribe from '@components/common/Subscribe';
 
