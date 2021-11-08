901f6e20776bdff1280bf425d707b6047f02e877
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-11-08 12:28:58 +0800

    Fix

--- src/components/bus/Payments/QPay/index.tsx
+++ src/components/bus/Payments/QPay/index.tsx
@@ -1,1 +1,11 @@
-export { default } from './QPay';
+import Image from 'next/image';
+export default function Payment() {
+  return (
+    <Image
+      src="/assets/qpay.jpg"
+      alt="Picture of the author"
+      width={150}
+      height={150}
+    />
+  );
+}
