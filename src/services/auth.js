74ed508345d46fe236dc4dcc183010ec5e4154c6
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-11-09 11:30:05 +0800

    Fix

--- src/services/auth.js
+++ src/services/auth.js
@@ -28,15 +28,8 @@ const AuthService = {
   },
 
   logout() {
-    return new Promise<void>(resolve => {
-      if (Client.defaults.headers.common) {
-        const headersCommon = Client.defaults.headers.common;
-        delete headersCommon.Authorization;
-        Client.defaults.headers.common = headersCommon;
-      }
-      AuthTokenStorageService.clear();
-      resolve();
-    });
+    AuthTokenStorageService.clear();
+    resolve();
   },
 
   isAuthenticated() {
