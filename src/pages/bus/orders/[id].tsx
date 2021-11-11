aa8cde802cf570432bde34b0a3b30538e4f31694
Erdenebileg Chinchuluun <jdkcsms@gmail.com>
2021-11-11 16:09:21 +0800

    Fix

--- src/pages/bus/orders/[id].tsx
+++ src/pages/bus/orders/[id].tsx
@@ -1,41 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PassengerInfo from '@components/bus/PassengerInfo';
 import Payments from '@components/bus/Payments';
-import PaymentCard from '@components/bus/PaymentCard';
 import { useQuery } from '@apollo/client';
 import { BUS_SCHEDULES_DETAIL_QUERY } from '@graphql/queries';
-import { message, Steps } from 'antd';
+import { Steps } from 'antd';
 import NavData from '@data/navData.json';
 import SelectSeats from '@components/bus/SelectSeats';
 import ContentWrapper from './style';
 import Layout from '@components/common/Layout';
-import StepCard from '@components/bus/StepCard';
 import SeatNav from '@components/bus/SeatNavbar';
 import { useRouter } from 'next/router';
-const { Step } = Steps;
-import s from './orders.module.scss';
-import ConfirmModal from '@components/common/ConfirmModal';
 import { useGlobalStore } from '@context/globalStore';
 import Loader from '@components/common/Loader';
-import AuthService from '@services/auth';
+
+const { Step } = Steps;
 
 export default function Payment() {
   const router = useRouter();
   const { current, setCurrent } = useGlobalStore();
-  const { selectedSeats, customers } = useGlobalStore();
   const { id } = router.query;
 
-  const {
-    data: scheduleDataDetail,
-    loading,
-    error,
-  } = useQuery(BUS_SCHEDULES_DETAIL_QUERY, {
-    variables: {
-      id: id,
-    },
-  });
+  const { data: scheduleDataDetail, loading } = useQuery(
+    BUS_SCHEDULES_DETAIL_QUERY,
+    {
+      variables: {
+        id: id,
+      },
+    }
+  );
 
-  if (error) return `Error! ${error.message}`;
   const scheduleDataResult =
     scheduleDataDetail === undefined ? '' : scheduleDataDetail.busSchedule;
 
