51eca48187216a8e13fe4bb518a9f15665f9c1db
Erdenebileg Chinchuluun <jdkcsms@gmail.com>
2021-11-12 16:43:09 +0800

    Fix

--- src/components/bus/PassengerInfo/index.tsx
+++ src/components/bus/PassengerInfo/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Input, Select } from 'antd';
 import registNo from '@data/registerNumber.json';
-import RegisterNumber from '@components/bus/PassengerInfo/RegisterNumber';
+// import RegisterNumber from '@components/bus/PassengerInfo/RegisterNumber';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
 import style from './PassengerInfo.module.scss';
 import { useGlobalStore } from '@context/globalStore';
 import {
@@ -18,7 +19,35 @@ export default function PassengerIfo({ datas }) {
   const [isCompoany, setIsCompany] = useState(true);
   const { customers, setCustomers } = useGlobalStore();
   const { selectedSeats, setSelectedSeats } = useGlobalStore();
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [values1, setValues1] = useState('A');
+  const [values2, setValues2] = useState('A');
   const { current, setCurrent } = useGlobalStore();
+
+  const handleReg1 = e => {
+    setIsOpen1(!isOpen1);
+    setIsOpen2(false);
+  };
+
+  const handleReg2 = e => {
+    setIsOpen2(!isOpen2);
+    setIsOpen1(false);
+  };
+
+  const handleValue1 = e => {
+    setValues1(e.target.value);
+    setIsOpen1(false);
+  };
+
+  const handleValue2 = e => {
+    setValues2(e.target.value);
+    setIsOpen2(false);
+  };
+
+  const handleRegister = e => {
+    console.log(e);
+  };
 
   const handleCompany = data => {
     let company = data == 0 ? true : false;
@@ -73,13 +102,11 @@ export default function PassengerIfo({ datas }) {
   const handlePassengerSurname = e => {
     selectedSeats[e.target.id - 1].lastName = e.target.value;
     setSelectedSeats(selectedSeats);
-    console.log(selectedSeats);
   };
 
   const handlePassengerFirstname = e => {
     selectedSeats[e.target.id - 1].firstName = e.target.value;
     setSelectedSeats(selectedSeats);
-    console.log(selectedSeats);
   };
 
   const onFinish = async values => {
@@ -107,10 +134,7 @@ export default function PassengerIfo({ datas }) {
                 <div className="w-full sm:grid grid-cols-2 px-4 pt-2 pb-4">
                   <div className={style.InfoForm}>
                     <div className={style.leftContent}>
-                      <label
-                        className={style.Label}
-                        htmlFor="RegisterNo"
-                      ></label>
+                      <label className={style.Label} htmlFor="type"></label>
                       <Form.Item name="type">
                         <Select onChange={handleCompany} defaultValue="0">
                           <Option value="0">Хувь хүн</Option>
@@ -145,7 +169,7 @@ export default function PassengerIfo({ datas }) {
                   </div>
                   <div className={style.InfoForm}>
                     <div className={style.rightContent}>
-                      <label className={style.Label} htmlFor="RegisterNo">
+                      <label className={style.Label} htmlFor="companyRegister">
                         Регистрийн дугаар
                       </label>
                       <Form.Item
@@ -193,22 +217,99 @@ export default function PassengerIfo({ datas }) {
                           <label className={style.Label} htmlFor="RegisterNo">
                             Регистрийн дугаар
                           </label>
-                          <Input
-                            onChange={handlePassengerSurname}
-                            id={i}
-                            className={style.input}
-                          />
+                          <Form.Item
+                            name="register"
+                            rules={[
+                              {
+                                pattern: PATTERN_PHONE,
+                                message: 'Регистерийн дугаар буруу байна!',
+                              },
+                              {
+                                required: true,
+                                message:
+                                  'Регистерийн дугаараа заавал бөглөнө үү!',
+                              },
+                            ]}
+                          >
+                            <div className="w-full">
+                              <div className="flex w-full">
+                                <button
+                                  className="flex justify-center pl-3 pr-1 border-0 items-center rounded-lg bg-bg w-16"
+                                  onClick={handleReg1}
+                                >
+                                  <h2 className="flex items-center text-cardDate font-semibold">
+                                    {values1}
+                                    {!isOpen1 ? (
+                                      <ChevronDownIcon className="text-secondary h-6 w-6" />
+                                    ) : (
+                                      <ChevronUpIcon className="text-secondary h-6 w-6" />
+                                    )}
+                                  </h2>
+                                </button>
+                                <button
+                                  className="flex justify-center border-0 pl-3 pr-1 items-center rounded-lg bg-bg mx-2 w-16"
+                                  onClick={handleReg2}
+                                >
+                                  <h2 className="flex items-center text-cardDate font-semibold">
+                                    {values2}
+                                    {!isOpen2 ? (
+                                      <ChevronDownIcon className="text-secondary h-6 w-6" />
+                                    ) : (
+                                      <ChevronUpIcon className="text-secondary h-6 w-6" />
+                                    )}
+                                  </h2>
+                                </button>
+                                <Input
+                                  className={style.input}
+                                  onChange={handleRegister}
+                                  placeholder="Регистерийн дугаар"
+                                />
+                              </div>
+                              {!isOpen1 ? (
+                                ''
+                              ) : (
+                                <div className="z-10 flex flex-wrap mt-2 bg-white ml-1 w-64 absolute shadow-md rounded-xl px-2 py-3">
+                                  {registNo.map(k => (
+                                    <button
+                                      value={k.uniCode}
+                                      onClick={handleValue1}
+                                      className="bg-bg text-cardDate font-semibold rounded-md h-9 w-10 m-1 hover:bg-register hover:text-white "
+                                    >
+                                      {k.uniCode}
+                                    </button>
+                                  ))}
+                                </div>
+                              )}
+                              {!isOpen2 ? (
+                                <h1></h1>
+                              ) : (
+                                <div>
+                                  <div className="z-10 ml-14 mt-2 flex flex-wrap bg-white absolute w-64 shadow-md rounded-xl p-2 py-3">
+                                    {registNo.map(k1 => (
+                                      <button
+                                        value={k1.uniCode}
+                                        onClick={handleValue2}
+                                        className="bg-bg text-cardDate font-semibold rounded-md h-9 w-10 m-1 hover:bg-register hover:text-white"
+                                      >
+                                        {k1.uniCode}
+                                      </button>
+                                    ))}
+                                  </div>
+                                </div>
+                              )}
+                            </div>
+                          </Form.Item>
                         </div>
                         <div className={style.leftContent}>
-                          <label className={style.Label} htmlFor="RegisterNo">
+                          <label className={style.Label} htmlFor="lastName">
                             Овог
                           </label>
                           <Form.Item
-                            name="lastname"
+                            name="lastName"
                             rules={[
                               {
                                 required: true,
-                                message: 'dsadsa',
+                                message: 'Зорчигчийн овгийг заавал бөглөнө үү!',
                               },
                             ]}
                           >
@@ -216,19 +317,20 @@ export default function PassengerIfo({ datas }) {
                               onChange={handlePassengerSurname}
                               id={i}
                               className={style.input}
+                              placeholder="Зорчигчийн овог"
                             />
                           </Form.Item>
                         </div>
                       </div>
                       <div className={style.InfoForm}>
                         <div className={style.rightContent}>
-                          <label className={style.Label} htmlFor="RegisterNo">
+                          <label className={style.Label} htmlFor="Vaccine">
                             Вакцинд хамрагдсан эсэх
                           </label>
-                          <p className={style.input}>Вакцинд хамрагдсан эсэх</p>
+                          <Input disabled className={style.input} />
                         </div>
                         <div className={style.rightContent}>
-                          <label className={style.Label} htmlFor="RegisterNo">
+                          <label className={style.Label} htmlFor="firstName">
                             Нэр
                           </label>
                           <Form.Item
@@ -236,7 +338,7 @@ export default function PassengerIfo({ datas }) {
                             rules={[
                               {
                                 required: true,
-                                message: 'Нэрээ заавал бөглөнө үү!',
+                                message: 'Зорчигчийн нэрийг заавал бөглөнө үү!',
                               },
                             ]}
                           >
