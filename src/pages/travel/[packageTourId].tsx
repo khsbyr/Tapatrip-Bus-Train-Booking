81373d856c5b9533a8f4168b11a7adfb85121fd0
Erdenebileg Chinchuluun <jdkcsms@gmail.com>
2021-11-17 15:19:54 +0800

    Fix

--- src/pages/travel/[packageTourId].tsx
+++ src/pages/travel/[packageTourId].tsx
@@ -8,7 +8,6 @@ import { postRequest } from '@lib/api';
 import { Carousel, Timeline } from 'antd';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 const breadcrumbRoutes = [
   {
     path: '/',
@@ -20,7 +19,6 @@ const breadcrumbRoutes = [
 ];
 
 export default function packageDetail({ NavData, PackTour }) {
-  const { t } = useTranslation();
   const [totalPrice, setTotalPrice] = useState(0);
   const [collectedPackages, setCollectedPackages] = useState([]);
   const [subPack, setSubPack] = useState('');
