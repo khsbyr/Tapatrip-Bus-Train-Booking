ccac91afe92d2f2d9561c908c58ee1ba89338747
Erdenebileg Chinchuluun <jdkcsms@gmail.com>
2021-12-08 17:42:48 +0800

    FixA

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -9,10 +9,9 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token =
-    AuthTokenStorageService.getAccessToken() === undefined
-      ? AuthTokenStorageService.getGuestToken()
-      : AuthTokenStorageService.getAccessToken();
+  const token = AuthTokenStorageService.getAccessToken()
+    ? AuthTokenStorageService.getAccessToken()
+    : AuthTokenStorageService.getGuestToken();
   // return the headers to the context so httpLink can read them
   return {
     headers: {
