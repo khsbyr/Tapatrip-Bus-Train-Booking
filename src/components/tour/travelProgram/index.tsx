973216e2597416fb45ab738dbd5832482ca64d11
khosbayar <khosbayar.b@dlp.mn>
2021-12-22 15:28:21 +0800

    fix

---
+++ src/components/tour/travelProgram/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TravelProgram = () => {
+  return (
+    <div className="max-w-7xl mx-auto grid justify-center py-12">
+      <h1 className="text-2xl font-bold">АЯЛАЛ, ХӨТӨЛБӨР</h1>
+
+      <div className="grid grid-cols-5 gap-x-4">
+        <img src="/assets/tourImages/1.jpg" />
+        <img src="/assets/tourImages/2.jpg" />
+        <img src="/assets/tourImages/3.jpg" />
+        <img src="/assets/tourImages/4.jpg" />
+        <img src="/assets/tourImages/5.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default TravelProgram;
--- src/pages/tour/index.tsx
+++ src/pages/tour/index.tsx
@@ -1,34 +1,20 @@
+import SeatNav from '@components/bus/seatNavbar';
 import Footer from '@components/common/footer';
-import HeaderBackground from '@components/common/headerBackground';
-import Navbar from '@components/common/navbar';
-import Layout from '@components/common/layout';
-import App from '@components/common/subscribe';
-import TapaService from '@components/common/tapaService';
+import styles from '@components/common/layout/layout.module.scss';
 import Company from '@data/company.json';
 import NavData from '@data/navData.json';
-import TapaServiceList from '@data/tapaServiceList.json';
-import Head from 'next/head';
-import styles from '@components/common/layout/layout.module.scss';
-import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Search from '@components/train/searchPanel';
-import { arrayFormat } from '@helpers/array-format';
-import { useQuery } from '@apollo/client';
-import { BUS_ALL_LOCATIONS_QUERY } from '@graphql/queries';
-import SeatNav from '@components/bus/seatNavbar';
+import Head from 'next/head';
+import React from 'react';
+import TravelProgram from '@components/tour/travelProgram';
 
 export default function Tour() {
-  const { data, loading, error } = useQuery(BUS_ALL_LOCATIONS_QUERY);
-
-  if (error) return `Error! ${error.message}`;
-  const startLocations = arrayFormat(data);
   return (
     <div>
       <Head>
         <title>Tapatrip - Online Travel Platform</title>
       </Head>
       <div className={styles.main}>
-        {/* <HeaderBackground /> */}
         <div className="relative">
           <div className="fixed z-20 w-screen top-0">
             <SeatNav />
@@ -37,20 +23,7 @@ export default function Tour() {
         <div className="mt-20">
           <img src="/assets/tourImages/top-bg.jpg" alt="Logo" />
         </div>
-        {/* <Navbar navbarData={NavData} /> */}
-        {/* <Search navbarData={NavData} startLocations={startLocations} /> */}
-        {/* <div className="bg-bg relative w-full mt-10">
-          <div className="bg-white max-w-7xl mx-auto p-5 rounded-lg">
-            <p className="text-center text-lg text-blue-500 font-medium">
-              Coming soon
-            </p>
-            <div className="flex justify-center -mt-10">
-              <img src="/assets/not_found.gif" alt="" width={'800'} />
-            </div>
-          </div>
-        </div> */}
-        {/* <App /> */}
-        <TapaService tapaServiceList={TapaServiceList} />
+        <TravelProgram />
         <Footer companyInfo={Company} />
       </div>
     </div>
