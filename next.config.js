6022b56e77bdb707fec1d995cc46138895a64bdc
Erdenebileg <jdkcsms@gmail.com>
2022-01-11 10:51:06 +0800

    Fix

--- next.config.js
+++ next.config.js
@@ -20,18 +20,46 @@ module.exports = phase => {
   return {
     i18n,
     reactStrictMode: true,
-    // async redirects() {
-    //   return [
-    //     {
-    //       source: '/',
-    //       destination: '/bus',
-    //       permanent: true,
-    //     },
-    //   ];
-    // },
+    compress: true,
     images: {
-      loader: 'imgix',
-      path: 'https://noop/',
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      minimumCacheTTL: 60,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    swcMinify: false,
+    productionBrowserSourceMaps: true,
+    future: {
+      strictPostcssConfiguration: true,
+    },
+    async headers() {
+      return [
+        {
+          // This works, and returns appropriate Response headers:
+          source: '/:all*(svg|jpg|png|webp)',
+          locale: false,
+          headers: [
+            {
+              key: 'Cache-Control',
+              value:
+                'public, max-age=31536000, s-maxage=31536000, stale-while-revalidate=31536000, must-revalidate',
+            },
+          ],
+        },
+        {
+          // This doesn't work for 'Cache-Control' key (works for others though):
+          source: '/_next/image(.*)',
+          locale: false,
+          headers: [
+            {
+              key: 'Cache-Control',
+              value:
+                'public, max-age=31536000, s-maxage=31536000, stale-while-revalidate=31536000, must-revalidate',
+            },
+          ],
+        },
+      ];
     },
   };
 };
