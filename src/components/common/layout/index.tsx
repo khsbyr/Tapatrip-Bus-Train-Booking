17769aff5ed2daa58c185307d0a16addb9b200e7
Erdenebileg <jdkcsms@gmail.com>
2022-02-11 17:24:40 +0800

    FixMeta

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -2,22 +2,21 @@ import Head from 'next/head';
 import styles from './layout.module.scss';
 import Company from '@data/company.json';
 import Footer from '@components/common/footer';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+  const router = useRouter();
   return (
     <>
       <Head>
+        <title>Tapatrip - Bus Booking System</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charSet="utf-8" />
-        <meta
-          name="google-site-verification"
-          content="jLGHUohzVujDpZVKJOgJScLlCCZMh3D-_8neGKul6IQ"
-        />
         <meta
           name="description"
+          key="description"
           content="автобус, билет захиалга, автобусны билет захиалга, автобусны билет захиалга үнэ, автобусны хуваарь үнэ тариф, Bus, Train"
         />
-        <meta name="title" content="Tapatrip - Bus Booking System" />
+        <meta name="title" key="title" content="Tapatrip - Bus Booking System" />
         <meta name="category" content="bus, travel, train" />
         <meta name="subject" content="bus" />
         <meta
@@ -29,7 +28,12 @@ export default function Layout({ children }) {
           key="ogtitle"
           content="Tapatrip - Bus Booking System"
         />
-        <meta property="og:locale" key="oglocale" content="mn_MN" />
+        <meta property="og:locale" key="og:locale" content="mn_MN" />
+        {router.asPath === '/' && (
+          <meta property="og:image" content="/images/fb_cover.jpg" />
+        )}
+        <meta property="fb:app_id" content="476931413433212" />
+        <meta property="fb:page_id" content="1421291734678399" />
         <meta property="og:email" content="crm@tapatrip.com" />
         <meta property="og:phone_number" content="75154444" />
         <meta
@@ -39,6 +43,10 @@ export default function Layout({ children }) {
         <meta property="og:locality" content="Ulaanbaatar" />
         <meta property="og:region" content="UB" />
         <meta property="og:country-name" content="Mongolia" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://www.bus.tapatrip.com" />
+        <meta name="twitter:title" content="TapaTrip" />
+        <meta name="twitter:image" content="/fb_cover.png" />
         <meta
           property="og:url"
           key="ogurl"
@@ -50,11 +58,10 @@ export default function Layout({ children }) {
           key="ogdesc"
           content="автобус, билет захиалга, автобусны билет захиалга, автобусны билет захиалга үнэ, автобусны хуваарь үнэ тариф, Bus, Train"
         />
-         <meta
-          property="og:image:secure_url"
-          content="https://tapatrip.com/images/fb_cover.jpg"
+        <meta
+          name="google-site-verification"
+          content="jLGHUohzVujDpZVKJOgJScLlCCZMh3D-_8neGKul6IQ"
         />
-        <title>Tapatrip - Bus Booking System</title>
       </Head>
       <main className={styles.main}>
         {children}
